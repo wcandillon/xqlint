cc444b2207d0ad2c0695d9761a95f0dcc2b55f12
ldoblies <ldoblies@ethz.ch>
2012-12-14 10:51:20 +0800

    CodeFormatter now caches current line as an object exported tests in codeformatter_test.js to files

--- tests/codeformatter_test.js
+++ tests/codeformatter_test.js
@@ -42,6 +42,9 @@ define(function(require, exports, module) {
   var JSONParseTreeHandler = requirejs('../lib/JSONParseTreeHandler').JSONParseTreeHandler;
   var CodeFormatter = requirejs('../lib/visitors/CodeFormatter').CodeFormatter;
 
+  var fs = require('fs');
+  var path = require('path');
+
   function testFormat(code, expected){
     var h = new JSONParseTreeHandler(code);
     var parser = new XQueryParser(code, h);
@@ -50,6 +53,7 @@ define(function(require, exports, module) {
     var codeFormatter = new CodeFormatter(ast);
     var formatted = codeFormatter.format();
     if (expected.trim() != formatted.trim()){
+      console.log("Input:\n" + code.trim());
       console.log("Expected:\n" + expected.trim());
       console.log("Formatted:\n" + formatted.trim());
     }
@@ -59,88 +63,37 @@ define(function(require, exports, module) {
 
   module.exports = {
 
-    name: "Code Formatter",
-
-    "test: simple code formatting": function() {
-      var code = "for $i in (let, for) return return";
-      var expected = "for $i in (let, for)\nreturn return";
-      var h = new JSONParseTreeHandler(code);
-      var parser = new XQueryParser(code, h);
-      parser.parse_XQuery();
-      var ast = h.getParseTree();
-      var codeFormatter = new CodeFormatter(ast);
-      var formatted = codeFormatter.format();
-      assert.equal(formatted, expected); 
-    },
-
-    "test: simple enclosed expr": function() {
-      var code = "<foo foo='{for $i in (1 to 10) return $i}' />";
-      var expected = code;
-      testFormat(code, expected);
-    },
-
-    "test: simple FLWOR": function() {
-      var code = "let $a := let $b := 1 return $b return $a";
-      var expected = "let $a := let $b := 1\n    return $b\nreturn $b";
-      testFormat(code, expected);    
-    },
-
-
-    "test: simple FLWOR 2": function() {
-      var code ="let $set1:=(3,2,1) let $set2 := (1,2,3) for $x in $set2 for $y in $set1 return if ($x = $y) then true() else false()";
-      var expected = "let $set1 := (3, 2, 1)\nlet $set2 := (1, 2, 3)\nfor $x in $set2\nfor $y in $set1\nreturn\n    if ($x = $y) then true() else false()";
-      testFormat(code, expected);
-    },
-
-    "test: sequence": function() {
-      var code = "( 1,   2,3, 4  )";
-      var expected = "(1, 2, 3, 4)";
-      testFormat(code, expected);
-    },
-
-    "test: arithmetic expression": function() {
-      var code = "1+2 -(3   * 4)/5";
-      var expected = "1 + 2 - (3 * 4) / 5";
-      testFormat(code, expected);
-    },
-
-    "test: if-then-else one line": function() {
-      var code = "if (true()) then 1 else 2";
-      var expected = code;
-      testFormat(code, expected);
-    },
-
-    "test: if-then-else wrapping": function() {
-      var code = "if (true()) then \"looooooooooooooooooooooooooooooooong\" else 1";
-      var expected = "if (true()) then\n    \"looooooooooooooooooooooooooooooooong\"\nelse\n    1";
-      testFormat(code, expected);
-    },
-
-    "test: functiondecl and call": function() {
-      var code = "declare function local:fun ($d as xs:decimal) as xs:decimal {$d}; local:fun(10)";
-      var expected = "declare function local:fun($d as xs:decimal)\nas xs:decimal\n{\n    $d\n};\nlocal:fun(10)";
-      testFormat(code, expected);
-    },
-
-    "test: typeswitch": function() {
-      var code = "typeswitch ((1)) case text() return 1 case element (a) return 2 default return 3";
-      var expected = "typeswitch ((1))\n    case text()\n        return 1\n    case element(a)\n        return 2\n    default\n        return 3";
-      testFormat(code, expected);
-    },
-
-    "test: operators (1)": function() {
-      var code = "1\t+1";
-      var expected = "1 + 1";
-      var h = new JSONParseTreeHandler(code);
-      var parser = new XQueryParser(code, h);
-      parser.parse_XQuery();
-      var ast = h.getParseTree();
-      var codeFormatter = new CodeFormatter(ast);
-      var formatted = codeFormatter.format();
-      assert.equal(formatted, expected); 
-    }
+    name: "Code Formatter"
 
   };
+
+  var testFiles = fs.readdirSync(path.resolve(__dirname, "queries/xqlint"));
+  
+  console.log("Parsing tests...");
+  for (var i = 0; i < testFiles.length; i++){
+
+    var testFile = path.resolve(__dirname, "queries/xqlint/" + testFiles[i]);
+    if (testFile.indexOf(".xq", testFile.length - 3) === -1){
+      continue;
+    }
+    var solFile = testFile.substring(0, testFile.length - 3) + ".txt";
+
+    var testFileContent = fs.readFileSync(testFile, "utf8");
+    var solFileContent = fs.readFileSync(solFile, "utf8");
+
+    var testName = "test: " + testFiles[i].substring(0, testFiles[i].length - 3);
+
+    console.log(testFile);
+
+    addTestCase(testName, testFileContent, solFileContent);
+  }
+
+  function addTestCase(name, code, expected){
+    module.exports[name] = function(){
+      testFormat(code,expected);
+    }
+  };
+
 });
 
 if (typeof module !== "undefined" && module === require.main) {
---
+++ tests/queries/xqlint/ifthenelsewrapping.xq
@@ -0,0 +1,1 @@
+if (true()) then \"looooooooooooooooooooooooooooooooong\" else 1
