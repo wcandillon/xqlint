54c4c630609c65f870796cae9df2cc4131c016e3
William Candillon <wcandillon@gmail.com>
2013-01-15 08:53:19 +0100

    Improvements in the semantic highlighting.

--- tests/lexer_test.js
+++ tests/lexer_test.js
@@ -223,7 +223,7 @@ module.exports = {
     },
     
     "test: XML ": function() {
-      var code = "for $i in false return true";
+      var code = "declare %an:foo $foo := 1;";
       var lines = code.split("\n");
       //var state = undefined;
       var expected = [
