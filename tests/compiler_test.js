e8ca8f11c71dc0918b140517f20422122c5c8e02
ldoblies <ldoblies@ethz.ch>
2013-02-27 15:31:47 +0100

    Translator improvements, additional static info for vardecl/functiondecl

---
+++ tests/compiler_test.js
@@ -0,0 +1,96 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Distributed under the BSD license:
+ *
+ * Copyright (c) 2010, Ajax.org B.V.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Ajax.org B.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+if (typeof process !== "undefined") {
+    require("amd-loader");
+}
+
+define(function(require, exports, module) {
+"use strict";
+
+var fs = require('fs');
+var assert = require("./assertions");
+
+var requirejs = require('../r');
+var Compiler = requirejs('../lib/Compiler').Compiler;
+
+
+function stringifyNoParents(json){
+  return stringify(json, ['parent']);
+}
+
+function stringify(json, ignoreKeys){
+return JSON.stringify(json, function(key, value) {
+    if (typeof value === 'object' && value !== null) {
+      if (ignoreKeys.indexOf(key) !== -1) {
+        return;
+      }
+    }
+    return value;
+  }, 2);
+}
+
+module.exports = {
+    
+    name: "Compiler/Translator",
+    
+    "test: functiondecl, vardecl, annotations, types": function() {
+      var code = 'declare function local:foo($p1 as xs:string, $p2 as xs:integer) as xs:string {1};\ndeclare %private variable $var as xs:string external := "3";\n{%public(1, "2") variable $a as xs:integer := 2, $b as xs:string; $a}';
+      //var code = 'let $x := 3 return $x';
+      var compiler = new Compiler();
+      var sctx = compiler.compile(code);
+      var markers = sctx.markers;
+      console.log(stringifyNoParents(sctx.sctx));
+      console.log(stringifyNoParents(sctx.outline));
+    },
+   
+   "test: undeclared variable in assignment": function() {
+      var code = "declare function local:test() {\nvariable $foo := $foo;\n$foo;\n};\nlocal:test()";
+      var compiler = new Compiler();
+      var sctx = compiler.compile(code);
+      var markers = sctx.markers;
+      console.log(stringifyNoParents(sctx.sctx));
+      console.log(stringifyNoParents(sctx.outline));
+    },
+   
+   "test: module variable": function() {
+      var code = "declare variable $a := 1; $a";
+      var compiler = new Compiler();
+      var sctx = compiler.compile(code);
+      var markers = sctx.markers;
+      console.log(stringifyNoParents(sctx.sctx));
+      console.log(stringifyNoParents(sctx.outline));
+    }
+};
+});
+
+if (typeof module !== "undefined" && module === require.main) {
+    require("asyncjs").test.testcase(module.exports).exec()
+}
