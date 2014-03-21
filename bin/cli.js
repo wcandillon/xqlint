5644eb239e5f23a52a3bdc2c21b93c2e07d07bef
William Candillon <wcandillon@gmail.com>
2014-03-21 23:18:03 +0100

    Update cli command

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var fs = require('fs');
+var ffs = require('final-fs');
+var path = require('path');
 var cli = require('commander');
 
 var XQLint = require('../lib/xqlint').XQLint;
@@ -16,21 +18,33 @@ var printAST = function(ast, indent){
 };
 
 cli
-.command('parse <file>')
-.description('Sign-in into 28.io.')
-.action(function(file) {
-    var source = fs.readFileSync(file, 'utf-8');
-    var linter = new XQLint(file, source);
-    var markers = linter.getMarkers();
-    var ast = linter.getAST();
-    //TreeOps.removeParentPtr(ast, '  ');
-    //printAST(ast, '  ');
-    //console.log(JSON.stringify(ast, null, 2));
-    if(markers.length === 0) {
-        console.log('File is OK');
+.command('parse <path>')
+.description('Check queries')
+.action(function(p) {
+    p = path.resolve(path.normalize(p));
+    var files = [];
+    if(fs.statSync(p).isFile()){
+        files.push(p);
     } else {
-        console.log(JSON.stringify(markers, null, 2));
+        var list = ffs.readdirRecursiveSync(p, true, p);
+        list.forEach(function(file){
+            if(['jq', 'xq'].indexOf(file.substring(file.length - 2)) !== -1) {
+                files.push(file);
+            }
+        });
     }
+    files.forEach(function(file){
+        console.log('Check ' + file);
+        var source = fs.readFileSync(file, 'utf-8');
+        var linter = new XQLint(file, source);
+        var markers = linter.getMarkers();
+        //var ast = linter.getAST();
+        if(markers.length === 0) {
+            console.log('File is OK');
+        } else {
+            console.log(JSON.stringify(markers, null, 2));
+        }
+    });
 });
 
 cli.version(pkg.version);
