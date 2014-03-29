33efe7ea306a5d4386b4168c65cdec30c4ab1d89
William Candillon <wcandillon@gmail.com>
2014-03-29 17:11:00 +0100

    Bug fixes

--- test/completion_test.js
+++ test/completion_test.js
@@ -8,23 +8,50 @@ var XQLint = require('../lib/xqlint').XQLint;
 
 vows.describe('Test Code Completion').addBatch({
     'test var (1)': function(){
+        var source = 'let $foo := 1 return $';
+        var linter = new XQLint(source);
+        var pos = { line: 0, col: source.length };
+        var proposals = linter.getCompletions(pos);
+        assert.equal(proposals.length, 1, 'Number of proposals');
+        assert.equal(proposals[0].name, '$foo', '$foo variable');
+    },
+    
+    'test var (2)': function(){
         var source = 'let $foo := 1 return $f';
         var linter = new XQLint(source);
         var pos = { line: 0, col: source.length };
         var proposals = linter.getCompletions(pos);
         assert.equal(proposals.length, 1, 'Number of proposals');
-        assert.equal(proposals[0].name, '$foo', '$foo var');
+        assert.equal(proposals[0].name, '$foo', '$foo variable');
 
     },
 
+    'test var (3)': function(){
+        var source = 'declare namespace ex = "http://www.example.com"; declare variable $ex:hello := 1; $';
+        var linter = new XQLint(source);
+        var pos = { line: 0, col: source.length };
+        var proposals = linter.getCompletions(pos);
+        assert.equal(proposals.length, 1, 'Number of proposals');
+        assert.equal(proposals[0].name, '$ex:hello', '$ex:hello variable');
+
+    },
+    
     'test expr (1)': function(){
         var source = 'declare function local:test($hello){ $hello }; l';
         var linter = new XQLint(source);
         var pos = { line: 0, col: source.length };
         var proposals = linter.getCompletions(pos);
-        assert.equal(proposals.length, 2, 'Number of proposals');
+        assert.equal(proposals.length, 1, 'Number of proposals');
+        assert.equal(proposals[0].name, 'local:', 'Prefix');
+    },
+    
+    'test expr (2)': function(){
+        var source = 'declare function local:test($hello){ $hello }; local:';
+        var linter = new XQLint(source);
+        var pos = { line: 0, col: source.length };
+        var proposals = linter.getCompletions(pos);
         console.log(proposals);
-        
-        //assert.equal(proposals[0].name, 'local:', 'Prefix');
+        assert.equal(proposals.length, 1, 'Number of proposals');
+        assert.equal(proposals[0].name, 'local:test($hello)', 'test function');
     }
 }).export(module);
