52493067d7539979c370ac9262fbfa462fe919ab
William Candillon <wcandillon@gmail.com>
2014-03-29 20:57:42 +0100

    Add URI completion

--- test/completion_test.js
+++ test/completion_test.js
@@ -4,6 +4,7 @@ var vows = require('vows');
 var assert = require('assert');
 //var fs = require('fs');
 
+var StaticContext = require('../lib/compiler/static_context').StaticContext;
 var XQLint = require('../lib/xqlint').XQLint;
 
 vows.describe('Test Code Completion').addBatch({
@@ -60,5 +61,42 @@ vows.describe('Test Code Completion').addBatch({
         var pos = { line: 0, col: source.length };
         var proposals = linter.getCompletions(pos);
         assert.equal(proposals.length, 2, 'Number of proposals');
+    },
+    
+    'test namespaces (1)': function(){
+        var p1 = 'import module namespace ns="';
+        var p2 = '";';
+        var sctx = new StaticContext();
+        sctx.availableModuleNamespaces.push('http://www.28msec.com/modules/http-reponse');
+        var linter = new XQLint(p1 + p2, { staticContext: sctx });
+        var pos = { line: 0, col: p1.length };
+        var proposals = linter.getCompletions(pos);
+        assert.equal(proposals.length, 1, 'Number of proposals');
+        assert.equal(proposals[0].name, 'http://www.28msec.com/modules/http-reponse', 'module list');
+    },
+    
+    'test namespaces (2)': function(){
+        var p1 = 'import module namespace ns="http://28msec.com/modules';
+        var p2 = '";';
+        var sctx = new StaticContext();
+        sctx.availableModuleNamespaces.push('http://www.28msec.com/modules/http-reponse');
+        sctx.availableModuleNamespaces.push('http://zorba.io/modules/reflection');
+        var linter = new XQLint(p1 + p2, { staticContext: sctx });
+        var pos = { line: 0, col: p1.length };
+        var proposals = linter.getCompletions(pos);
+        assert.equal(proposals.length, 0, 'Number of proposals');
+    },
+    
+    'test namespaces (3)': function(){
+        var p1 = 'import module namespace ns="http://www.28msec.com/modules';
+        var p2 = '";';
+        var sctx = new StaticContext();
+        sctx.availableModuleNamespaces.push('http://www.28msec.com/modules/http-reponse');
+        sctx.availableModuleNamespaces.push('http://zorba.io/modules/reflection');
+        var linter = new XQLint(p1 + p2, { staticContext: sctx });
+        var pos = { line: 0, col: p1.length };
+        var proposals = linter.getCompletions(pos);
+        assert.equal(proposals.length, 1, 'Number of proposals');
+        assert.equal(proposals[0].name, 'http://www.28msec.com/modules/http-reponse', 'module list');
     }
 }).export(module);
