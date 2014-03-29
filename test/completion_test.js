616bf6d8efd8a4ccba4f42edabcf0d42fd1fa340
William Candillon <wcandillon@gmail.com>
2014-03-29 16:46:16 +0100

    Add prefix completion

--- test/completion_test.js
+++ test/completion_test.js
@@ -2,16 +2,27 @@
 
 var vows = require('vows');
 var assert = require('assert');
-var fs = require('fs');
+//var fs = require('fs');
 
 var XQLint = require('../lib/xqlint').XQLint;
 
 vows.describe('Test Code Completion').addBatch({
     'test var (1)': function(){
-        var source = 'let $foo := 1 return $';
-        var linter = new XQLint('test', 'let $foo := 1 return $');
-        var pos = { sl: 0, el: 0, sc: source.length-1, ec: source.length-1};
-        var result = linter.getCompletions(pos);
-        console.log(result);
+        var source = 'let $foo := 1 return $f';
+        var linter = new XQLint(source);
+        var pos = { line: 0, col: source.length };
+        var proposals = linter.getCompletions(pos);
+        assert.equal(proposals.length, 1, 'Number of proposals');
+        assert.equal(proposals[0].name, '$foo', '$foo var');
+
+    },
+
+    'test expr (1)': function(){
+        var source = 'declare function local:test($hello){ $hello }; l';
+        var linter = new XQLint(source);
+        var pos = { line: 0, col: source.length };
+        var proposals = linter.getCompletions(pos);
+        assert.equal(proposals.length, 1, 'Number of proposals');
+        assert.equal(proposals[0].name, 'local:', 'Prefix');
     }
 }).export(module);
